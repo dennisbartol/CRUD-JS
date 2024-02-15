e1df0fb27b08bd5351fa0e9b02fb3f988f4eaafd
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-02-15 17:36:14 +0100

    Update print.js

--- print.js
+++ print.js
@@ -1,1 +1,13 @@
+const printTodoList = () => {
+  const printWindow = window.open('', '_blank');
 
+  let printContent = '<h2>To Do List</h2>'; 
+  printContent += '<ul>';
+  todos.forEach(todo => {
+    printContent += '<li>${todo.text}</li>';
+  });
+  printContent += '</ul>';
+
+
+
+}
